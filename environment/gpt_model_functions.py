22395c989ff55d8262cd41fb6a026e00faa4455b
courtgibson13 <gibsonce@umich.edu>
2024-07-06 19:11:35 -0400

    WIP endpoint data ingestion process

--- environment/gpt_model_functions.py
+++ environment/gpt_model_functions.py
@@ -7,7 +7,74 @@ from environment import env
 config = env.env()
 
 api_base_url = 'https://developer.nps.gov/api/v1/'
+parkcode_to_park = pd.read_csv(config['root']+'parkcode_to_park.csv')
+parkcode_to_park = dict(zip(parkcode_to_park['parkCode'], parkcode_to_park['fullName']))
 
+state_to_code = {
+    "Alabama": "AL",
+    "Alaska": "AK",
+    "Arizona": "AZ",
+    "Arkansas": "AR",
+    "California": "CA",
+    "Colorado": "CO",
+    "Connecticut": "CT",
+    "Delaware": "DE",
+    "Florida": "FL",
+    "Georgia": "GA",
+    "Hawaii": "HI",
+    "Idaho": "ID",
+    "Illinois": "IL",
+    "Indiana": "IN",
+    "Iowa": "IA",
+    "Kansas": "KS",
+    "Kentucky": "KY",
+    "Louisiana": "LA",
+    "Maine": "ME",
+    "Maryland": "MD",
+    "Massachusetts": "MA",
+    "Michigan": "MI",
+    "Minnesota": "MN",
+    "Mississippi": "MS",
+    "Missouri": "MO",
+    "Montana": "MT",
+    "Nebraska": "NE",
+    "Nevada": "NV",
+    "New Hampshire": "NH",
+    "New Jersey": "NJ",
+    "New Mexico": "NM",
+    "New York": "NY",
+    "North Carolina": "NC",
+    "North Dakota": "ND",
+    "Ohio": "OH",
+    "Oklahoma": "OK",
+    "Oregon": "OR",
+    "Pennsylvania": "PA",
+    "Rhode Island": "RI",
+    "South Carolina": "SC",
+    "South Dakota": "SD",
+    "Tennessee": "TN",
+    "Texas": "TX",
+    "Utah": "UT",
+    "Vermont": "VT",
+    "Virginia": "VA",
+    "Washington": "WA",
+    "West Virginia": "WV",
+    "Wisconsin": "WI",
+    "Wyoming": "WY",
+    "District of Columbia": "DC",
+    "American Samoa": "AS",
+    "Guam": "GU",
+    "Northern Mariana Islands": "MP",
+    "Puerto Rico": "PR",
+    "United States Minor Outlying Islands": "UM",
+    "U.S. Virgin Islands": "VI",
+}
+    
+# invert the dictionary
+code_to_stat = dict(map(reversed, state_to_code.items()))
+
+def statecode_mapping(state_code):
+    return code_to_stat[state_code]
 
 def handle_query(query, model, client, max_tokens):
     """
@@ -114,6 +181,10 @@ def api_call(query):
         temp_df = pd.DataFrame(responses[0])    
         addresses_df = pd.json_normalize(temp_df['addresses'])
         output = pd.concat([temp_df.drop(columns=['addresses']), addresses_df], axis=1) 
+        if intent == 'state':
+            output = f"{output['fullName'][0]} is located in {statecode_mapping(output['state'][0])}"
+        if intent == 'address':
+            output = f"{output['fullName'][0]} is located at {output['line1'][0]} " + (f"{output['line2'][0]} " if output['line2'][0] else "") + f"in {output['city'][0]}, {statecode_mapping(output['state'][0])} {output['postalCode'][0]}"
     else:
         output = pd.DataFrame(responses)    
 
