56fe7b3dd6beadaf5b37b9525b38028b53f461a6
Lauralyn Curry-Leech <llcl@umich.edu>
2024-07-23 17:56:10 -0400

    updates

---
+++ 04_nps_park_pal/park_pal_non_gpt.py
@@ -0,0 +1,32 @@
+#Hosting Locally:
+import sys
+sys.path.insert(0,'../')
+from environment import env
+from model_functions.nps_model_functions import *
+config = env.env()
+
+from flask import Flask, request, jsonify, render_template
+# *ChatGPT created the following flask application.
+# ChatGPT also constructed the index.html file.
+# Our team customized the style code within the index.html.
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    user_input = request.json.get("message")
+    if not user_input:
+        return jsonify({"response": "Please provide a message."})
+
+    try:
+        # Calls the OpenAI Fine-tuned GPT models (each run will charge the account)
+        output = api_call(user_input)
+    except:
+        output = "I specialize only in queries related to amenities, events, alerts, park fees, park locations, and park descriptions. Please clarify your question."
+    return jsonify({"response": output})
+
+if __name__ == "__main__":
+    app.run(port=8000)
